c07fa2aef866ddf3981f902ed5f3e600c6e96e13
Jonathancty <jc@enochan.net>
2024-09-16 21:30:44 +0100

    updated Back_end
    made half of the login page logic

---
+++ Back_end/controllers/userControllers.js
@@ -0,0 +1,124 @@
+const HttpError = require("../models/errorModel");
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+// ================================================= REGISTER A USER =================================================
+// POST : api/users/register
+// UNPROTECTED
+const registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password1, password2 } = req.body;
+    if (!name || !email || !password1 || !password2) {
+      return next(new HttpError("Please fill in all fields", 422));
+    }
+
+    const newEmail = email.toLowerCase();
+    const emailExists = await User.findOne({ email: newEmail });
+    if (emailExists) {
+      return next(new HttpError("Email already exists", 422));
+    }
+
+    if (password1.trim().length < 6) {
+      return next(new HttpError("Password must be at least 6 characters", 422));
+    }
+
+    if (password1 !== password2) {
+      return next(new HttpError("Passwords do not match", 422));
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password1, salt);
+    const newUser = await User.create({
+      name,
+      email: newEmail,
+      password: hashedPassword,
+    });
+    res.status(201).json(`New User ${newUser.email} has been registered`);
+  } catch (error) {
+    return next(
+      new HttpError("User Registration failed, please try again", 422)
+    );
+  }
+};
+
+// ================================================= LOGIN A REGISTERED USER =================================================
+// POST : api/users/login
+// UNPROTECTED
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return next(new HttpError("Fill in all fields", 422));
+    }
+    const newEmail = email.toLowerCase();
+    const user = await User.findOne({ email: newEmail });
+    if (!user) {
+      return next(new HttpError("Invalid credentials", 422));
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return next(new HttpError("Invalid credentials", 422));
+    }
+    const { _id: id, name } = user;
+    const token = jwt.sign({ id, name }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.status(200).json({ token, id, name });
+  } catch (error) {
+    return next(new HttpError("Login failed, please try again", 422));
+  }
+};
+
+// ================================================= USER PROFILE =================================================
+// POST : api/users/:id
+// PROTECTED
+const getUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id).select("-password");
+    if (!user) {
+      return next(new HttpError("User not found", 404));
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    return next(new HttpError("User not found", 404));
+  }
+};
+
+// ================================================= CHANGE USER AVATAR (profile pic) =================================================
+// POST : api/users/change-avatar
+// PROTECTED
+const changeAvatar = async (req, res, next) => {
+  res.json("Change avatar");
+};
+
+// ================================================= EDIT USER DETAILS (fromn profile) =================================================
+// POST : api/users/edit-user
+// PROTECTED
+const editUser = async (req, res, next) => {
+  res.json("Edit user details");
+};
+
+// ================================================= GET AUTHORS =================================================
+// POST : api/users/
+// UNPROTECTED
+const getAuthors = async (req, res, next) => {
+  try {
+    const authors = await User.find().select("-password");
+    if (!authors) {
+      return next(new HttpError("No authors found", 404));
+    }
+    res.status(200).json(authors);
+  } catch (error) {
+    return next(new HttpError("No authors found", 404));
+  }
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getUser,
+  changeAvatar,
+  editUser,
+  getAuthors,
+};
