479d742a23bdf7e11f3be0eadd6c9dd3c4d20af0
Jonathancty <jc@enochan.net>
2024-09-26 18:49:29 +0100

    Update index.js

--- Back_end/index.js
+++ Back_end/index.js
@@ -40,7 +40,7 @@ app.use("/api/posts", postRoutes);
 app.use(express.static(path.join(__dirname, "../Front_end")));
 
 // Catch-all route to serve `main.jsx` for all other routes
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../Front_end", "index.html"));
 });
 
